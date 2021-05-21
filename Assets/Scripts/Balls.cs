9c314ea86c8ba76164f0f41f060e26a1695de9ee
Seyidahmetyogurtcu <yogurtcu00@hotmail.com>
2021-05-21 10:35:12 +0300

    First Comment
    Add to Github

---
+++ Assets/Scripts/Balls.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Balls : MonoBehaviour
+{
+    private void OnMouseDown()
+    { 
+        GameObject.Find("GameManager").GetComponent<GameManager>().ScoreUp();
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject winText;
+    public GameObject restartButton;
+    int score = 0;
+
+    public void ScoreUp()
+    {
+        score++;
+        if (score>=8)
+        {
+            win();
+        }
+    }
+    private void win()
+    {
+        winText.SetActive(true);
+        restartButton.SetActive(true);
+    }
+    public  void Restart()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+}
